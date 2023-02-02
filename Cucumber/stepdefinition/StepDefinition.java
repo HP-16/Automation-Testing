f3bb0eb93ddf3d8bdcdf26c3caea595c3b199973
Hariprasath_P <45896823+HP-16@users.noreply.github.com>
2023-02-02 11:21:56 +0530

    Add files via upload

---
+++ Cucumber/stepdefinition/StepDefinition.java
@@ -0,0 +1,39 @@
+package org.stepdefinition;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StepDefinition{
+	WebDriver driver;
+	@Given("User launches facebook web application")
+	public void user_launches_facebook_web_application() {
+	    WebDriverManager.chromedriver().setup();
+	    driver = new ChromeDriver();
+	    driver.get("https://www.facebook.com/");
+	}
+	
+	@When("User enters valid {string} and valid {string}")
+	public void user_enters_valid_and_valid(String uName, String pwd) {
+		driver.findElement(By.id("email")).sendKeys(uName);
+	    driver.findElement(By.id("pass")).sendKeys(pwd);
+	}
+
+	@When("User clicks Login Button")
+	public void user_clicks_Login_Button() {
+	    driver.findElement(By.name("login")).click();
+	}
+
+	@Then("Error message should be displayed")
+	public void error_message_should_be_displayed() {
+	    String errorMsg = driver.findElement(By.className("_9ay7")).getText();
+	    System.out.println("Facebook error message : "+errorMsg);
+	}
+
+}
